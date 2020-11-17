0948eba7fafe0c297b7dcf6e4fef6e5bf8c558dc
Marcin Złotek <marcin.zlotek@student.put.poznan.pl>
2020-11-17 12:41:57 +0100

    add new project

---
+++ LZ77/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LZ77
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string InputName = string.Empty;
+            string OutputName = "lz77default";
+            if(args.Length == 1)
+            {
+                InputName = args[0];
+                LZ77Manager lz77 = new LZ77Manager();
+                lz77.CompressStream()
+            }
+            else
+            {
+                if(args.Length == 2)
+                {
+                    InputName = args[0];
+                    OutputName = args[1];
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
